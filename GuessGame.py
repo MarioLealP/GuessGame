2d653ff4eef3b488ab05ccb4b30f753fdc58b434
Mário Leal <lealmario47@gmail.com>
2020-11-18 14:59:42 +0000

    Create GuessGame.py

---
+++ GuessGame.py
@@ -0,0 +1,18 @@
+import random
+ranint = random.randint(1, 100)
+mynum = None
+while mynum is not ranint:
+    print("Introduza o numero:")
+    try:
+        mynum = input()
+        mynum = int(mynum)
+    except ValueError:
+        print("Não foi introduzido um numero")
+    else:
+            if mynum > ranint:
+                print("O numero introduzido é maior que o numero certo")
+            elif mynum < ranint:
+                print("O numero introduzido é menor que o numero certo")
+            else:
+                break
+print("O numero esta certo")
