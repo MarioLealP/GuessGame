aa8d54867de15ed34ae00b21eb8a4ee7555299e2
Mário Leal <lealmario47@gmail.com>
2020-11-23 15:34:14 +0000

    update

--- GuessGame.py
+++ GuessGame.py
@@ -1,6 +1,8 @@
 import random
 ranint = random.randint(1, 100)
 mynum = None
+guess = 0
+print(ranint)
 while mynum is not ranint:
     print("Introduza o numero:")
     try:
@@ -9,10 +11,13 @@ while mynum is not ranint:
     except ValueError:
         print("Não foi introduzido um numero")
     else:
-        if mynum > ranint:
-            print("O numero introduzido é maior que o numero certo")
+        if guess > 10:
+            print("Não tem mais tentativas")
+            break
         elif mynum < ranint:
             print("O numero introduzido é menor que o numero certo")
+        elif mynum > ranint:
+            print("O numero introduzido é maior que o numero certo")
         else:
-            break
-print("O numero esta certo")
+            print("O numero esta certo")
+    guess += 1
