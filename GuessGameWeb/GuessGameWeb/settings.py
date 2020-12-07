0a7e73bf9d07bc212571b47ae6836c55ffef99a4
Mário Leal <lealmario47@gmail.com>
2020-12-07 17:05:56 +0000

    Update

--- GuessGameWeb/GuessGameWeb/settings.py
+++ GuessGameWeb/GuessGameWeb/settings.py
@@ -11,7 +11,9 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 from dotenv import load_dotenv
 from pathlib import Path
+import os
 load_dotenv()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
--- GuessGameWeb/GuessNumb/models.py
+++ GuessGameWeb/GuessNumb/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+
+class Guess(models.Model):
+    GuessInput = models.CharField(max_length=5)
